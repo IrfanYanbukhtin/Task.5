a17e4d91fb8c5d5d499be96e21c431bc57eac1d3
irfan <irfanfy@code.edu.az.com>
2022-12-28 20:51:12 +0400

    task5

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("range a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("range b:");
+            int b = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = a; i <= b; i++)
+            {
+                if (Math.Sqrt(i) == (int)Math.Sqrt(i))
+                {
+                    count++;
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine($"count {count}");
+
+
+
+        }
+}
+}
